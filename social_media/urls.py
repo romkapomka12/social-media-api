7282d473d7a5383f35451f5bebd899b975c28d1d
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-08 15:21:15 +0200

    has defined the URL path for your web application. created and registered routes for my API using the DefaultRouter class. installed the Django Rest framework

---
+++ social_media/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from social_media.views import UserViewSet, MessageViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+
+router.register("users", UserViewSet)
+router.register("posts", PostViewSet)
+router.register("message", MessageViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
+
+
+app_name = "social_media"
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -11,12 +11,22 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path(
+        "api/social_media/",
+        include("social_media.urls", namespace="social_media"),
+    ),
+    path("api/user/", include("user.urls.py", namespace="user")),
+    path("__debug__/", include("debug_toolbar.urls")),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
