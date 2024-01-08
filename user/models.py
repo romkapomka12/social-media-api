b21320e68b5cd8214becd3315e4300da92449f23
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-08 13:04:05 +0200

    added the first emigration of models

--- user/models.py
+++ user/models.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
-from post.models import Post, Discussion
+# from social_media.models import Post, Discussion
 
 
 class UserProfile(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
-    username = models.CharField(max_length=60, blank=True)
+    username = models.CharField(max_length=60, blank=True, unique=True)
     full_name = models.CharField(max_length=200, null=True)
     my_location = models.CharField(max_length=60, blank=True)
     my_status = models.TextField(null=True, blank=True)
@@ -15,10 +15,10 @@ class UserProfile(AbstractUser):
     )
     profile_pic = models.ImageField(blank=True, null=True, default="default.png")
     my_liked_posts = models.ManyToManyField(
-        Post, blank=True, related_name="users_liked"
+        "social_media.Post", blank=True, related_name="users_liked"
     )
     my_liked_comments = models.ManyToManyField(
-        Discussion, blank=True, related_name="users_liked"
+        "social_media.Discussion", blank=True, related_name="users_liked"
     )
 
     class Meta:
