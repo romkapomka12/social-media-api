31453563c5cab8e56cf81d95b91e8c86ca5da97f
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-08 15:17:22 +0200

    defined classes for the view (ViewSet) - UserViewSet, PostViewSet and MessageViewSet, which use the ModelViewSet model.

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
+from social_media.models import Post, Message
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.select_related("user")
+
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
