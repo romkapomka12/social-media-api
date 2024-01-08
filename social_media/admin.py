30a781ec0987946f55d38ce33d5502ddb1756adc
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-08 12:48:21 +0200

    registration of models in the admin

---
+++ social_media/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from social_media.models import Post, Discussion, Message
+
+admin.site.register(Post)
+admin.site.register(Discussion)
+admin.site.register(Message)
