c3af9e126cfe9f8a8d20e784f9078d61a26505d0
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-08 12:12:51 +0200

    creating models Post,Discussion,Message  for the post application. Changing the default user and overriding it. Built-in user model.

---
+++ post/models.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.db import models
+
+from user.models import UserProfile
+
+
+class Post(models.Model):
+    image = models.ImageField(blank=False, null=False)
+    location = models.TextField(null=True)
+    description = models.TextField(null=True, max_length=144)
+    tag_people = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="tags", blank=True
+    )
+    hash_tags = models.CharField(max_length=100, null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(UserProfile, related_name="liked_posts", blank=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+
+    def total_likes(self):
+        return self.likes.count()
+
+    class Meta:
+        ordering = ["created_at"]
+
+
+class Discussion(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="discussions")
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    receiver = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="received_messages"
+    )
+    body = models.TextField(null=True, blank=True)
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"{self.sender.username} and {self.receiver.username}"
+
+    class Meta:
+        ordering = ["timestamp"]
