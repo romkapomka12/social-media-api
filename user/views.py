80085794e7e659853fdc3798aa4de3a42ee5e4ee
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-08 15:18:12 +0200

    defined a CreateUserview class that uses generics.CreateAPIView to implement the creation of new users

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+    def get_object(self):
+        return self.request.user
