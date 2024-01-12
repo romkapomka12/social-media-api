cee2178338a1d7ebe6033736f0afa6a30a55a923
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-12 09:43:25 +0200

    added UserListSerializer and AuthTokenSerializer   which are used to authenticate the user

--- social_media/serializers.py
+++ social_media/serializers.py
@@ -1,25 +1,67 @@
+from datetime import datetime, date
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from social_media.models import Post, Message
 
 
-class PostListSerializer(serializers.Serializer):
+class PostListSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Post
-        fields = ("id", "created_at", "description", "image", "author")
+        fields = (
+            "id",
+            "author",
+            "image",
+            "description",
+            "likes",
+        )
         read_only_fields = ["author"]
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    days_since_created = serializers.SerializerMethodField()
+    hash_tags = serializers.CharField(max_length=50, allow_blank=True, default="")
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "image",
+            "location",
+            "description",
+            "tag_people",
+            "hash_tags",
+            "likes",
+            "author",
+            "days_since_created",
+        )
+        read_only_fields = ["author"]
+
+    def get_days_since_created(self, obj):
+        days_passed = (date.today() - obj.created_at.date()).days
+        if days_passed == 0:
+            return f"today"
+        return f"{days_passed} days ago"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        hash_tags = representation.get("hash_tags", "")
+        if hash_tags:
+            tags_list = hash_tags.split(",")
+            formatted_tags = ["#" + tag.strip() for tag in tags_list]
+            representation["hash_tags"] = ", ".join(formatted_tags)
+
+        return representation
+
+
 
 
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = (
-            "id",
-            "username",
-            "full_name",
-            "profile_pic"
-        )
+        fields = ("id", "username", "full_name", "profile_pic")
 
 
 class MessageListSerializer(serializers.ModelSerializer):
@@ -27,4 +69,3 @@ class MessageListSerializer(serializers.ModelSerializer):
         model = Message
         fields = ("id", "sender", "receiver", "body", "timestamp")
         # read_only_fields = ["body"]
-
--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -9,20 +10,50 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 6}}
 
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
 
-class ManageUserSerializer(UserSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = (
-            "id",
-            "email",
-            "username",
-            "full_name",
-            "my_status",
-            "profile_pic",
-            "is_staff",
-        )
-        read_only_fields = (
-            "is_staff",
-            "profile_pic",
-        )
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, set the password correctly and return it"""
+
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(label="Email")
+    password = serializers.CharField(
+        label="Password",
+        style={"input_type": "password"},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                email=email,
+                password=password
+            )
+            if not user:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+            if not user.is_active:
+                msg = _("User account is disabled.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code="authorization")
+
+        return {"user": user}
