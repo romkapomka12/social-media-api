78a99ab544448953a283dafaea2149369f599b64
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-01-15 14:49:00 +0200

    the path "/me/password-change/" is bound to the password change view and the second path "/login/" is bound to the token generation view

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from rest_framework import views
 
-from user.views import CreateUserView, ManageUserView
+from user.views import (
+    CreateUserView,
+    ManageUserView,
+    UpdatePasswordView,
+    CreateTokenView,
+)
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateUserView.as_view(), name="token_obtain_pair"),
+    path("login/", CreateTokenView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("me/password-change/", UpdatePasswordView.as_view(), name="password-change"),
 ]
 
 app_name = "user"
